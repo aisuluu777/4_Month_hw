d82daa74e1f272848baf1c7ed2ec2ea389debee6
aisuluu777 <a30139706@gmail.com>
2025-02-07 15:54:41 +0600

    completed hw2

--- books/models.py
+++ books/models.py
@@ -13,7 +13,7 @@ class BookModel(models.Model):
     image = models.ImageField(upload_to='books/', verbose_name='Загрузите фотографию')
     title = models.CharField(max_length=100, verbose_name='Введите название книги')
     description = models.TextField(verbose_name='Введите краткое описания книги', blank=True)
-    price = models.PositiveIntegerField(verbose_name='Введите название', default=250)
+    price = models.PositiveIntegerField(verbose_name='Введите цену', default=250)
     released_date = models.DateField(verbose_name='Укажите дату выпуска')
     genre = models.CharField(max_length=15 ,choices=GENRE_CHOICES, default='CLASSIC',
                              verbose_name='Выберите жанр')
